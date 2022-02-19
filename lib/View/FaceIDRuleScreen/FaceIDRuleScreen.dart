2870b724460b5f566d15159a85fb96a80ae81ae3
starmarketing <emad.hassan@starmarketingonline.com>
2022-02-19 19:24:51 +0500

    fixes

--- lib/View/FaceIDRuleScreen/FaceIDRuleScreen.dart
+++ lib/View/FaceIDRuleScreen/FaceIDRuleScreen.dart
@@ -12,6 +12,7 @@ class FaceIDRuleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
